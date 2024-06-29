a20d11d1f312c5c52390af8c3f9bed2e54205341
mufidu <mufid.to@gmail.com>
2024-06-29 18:55:18 +0700

    Fix sequelize error in member model

--- models/member.js
+++ models/member.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Member.hasMany(models.Book, {
+      Member.belongsToMany(models.Book, {
         through: models.MemberBook,
       });
     }
